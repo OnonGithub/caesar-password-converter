2dda11fefcd7e77c4b34a48eb1760cf02d9c3310
faqriansyah <ifaqriansyah@gmail.com>
2024-07-24 08:48:18 +0800

    decryptor done

--- src/main/java/org/dingco/encryption/Chiper.java
+++ src/main/java/org/dingco/encryption/Chiper.java
@@ -4,20 +4,42 @@ public class Chiper {
     private static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static int ALPHABET_LENGTH = ALPHABET.length();
 
+    public static void checkLength() {
+        System.out.println(ALPHABET_LENGTH);
+    }
+
     public static String encrypt(String word, int shift) {
         word = word.toUpperCase();
         StringBuilder encrypted = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char characterInput = word.charAt(i);
-            if(Validation.validate(characterInput)) {
+            if (Validation.validate(characterInput)) {
                 encrypted.append(characterInput);
                 continue;
-            };
+            }
+            ;
             int alphabetIndex = ALPHABET.indexOf(characterInput);
             int encryptedIndex = (alphabetIndex + shift) % ALPHABET_LENGTH;
-//            if (encryptedIndex < 0) {
-//                encryptedIndex += ALPHABET_LENGTH;
-//            }
+            char encryptedChar = ALPHABET.charAt(encryptedIndex);
+            encrypted.append(encryptedChar);
+        }
+        return encrypted.toString();
+    }
+
+    public static String decrypt(String word, int shift) {
+        word = word.toUpperCase();
+        StringBuilder encrypted = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char characterInput = word.charAt(i);
+            if (Validation.validate(characterInput)) {
+                encrypted.append(characterInput);
+                continue;
+            }
+            int alphabetIndex = ALPHABET.indexOf(characterInput);
+            int encryptedIndex = alphabetIndex - shift;
+            if (encryptedIndex < 0) {
+                encryptedIndex = 25;
+            }
             char encryptedChar = ALPHABET.charAt(encryptedIndex);
             encrypted.append(encryptedChar);
         }
--- src/test/java/org/dingco/encryption/ChiperTest.java
+++ src/test/java/org/dingco/encryption/ChiperTest.java
@@ -1,4 +1,11 @@
 package org.dingco.encryption;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ChiperTest {
+    @Test
+    void testingEncryption() {
+        Assertions.assertEquals("C", Chiper.decrypt("C", 2));
+    }
 }
